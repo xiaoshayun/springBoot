f35af4bbc1b4376768a75ae440c26d24c0efc2d8
pengy <734908907@qq.com>
2017-07-18 17:08:48 +0800

    sql脚本的加入

---
+++ xiaoyuntest.sql
@@ -0,0 +1,109 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50538
+Source Host           : localhost:3306
+Source Database       : xiaoyuntest
+
+Target Server Type    : MYSQL
+Target Server Version : 50538
+File Encoding         : 65001
+
+Date: 2017-07-18 17:07:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `descritpion` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `method` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+INSERT INTO `sys_permission` VALUES ('1', 'ROLE_HOME', 'home', '/', null, 'GET');
+INSERT INTO `sys_permission` VALUES ('2', 'ROLE_ADMIN', 'ABel', '/admin', null, 'POST');
+INSERT INTO `sys_permission` VALUES ('3', 'ROLE_USER_GET', 'user', '/user/**', null, 'GET');
+INSERT INTO `sys_permission` VALUES ('4', 'ROLE_USER_POST', 'user', '/user/**', null, 'POST');
+INSERT INTO `sys_permission` VALUES ('5', 'ROLE_USER_PUT', 'user', '/user/**', null, 'PUT');
+INSERT INTO `sys_permission` VALUES ('6', 'ROLE_USER_ALL', 'user', '/user/**', null, 'ALL');
+
+-- ----------------------------
+-- Table structure for sys_permission_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission_role`;
+CREATE TABLE `sys_permission_role` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `permission_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_permission_role
+-- ----------------------------
+INSERT INTO `sys_permission_role` VALUES ('1', '1', '1');
+INSERT INTO `sys_permission_role` VALUES ('2', '1', '2');
+INSERT INTO `sys_permission_role` VALUES ('3', '2', '1');
+INSERT INTO `sys_permission_role` VALUES ('4', '1', '6');
+INSERT INTO `sys_permission_role` VALUES ('5', '2', '1');
+INSERT INTO `sys_permission_role` VALUES ('6', '2', '3');
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', 'ROLE_ADMIN');
+INSERT INTO `sys_role` VALUES ('2', 'ROLE_USER');
+
+-- ----------------------------
+-- Table structure for sys_role_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_user`;
+CREATE TABLE `sys_role_user` (
+  `SYS_USER_ID` int(11) DEFAULT NULL,
+  `Sys_Role_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role_user
+-- ----------------------------
+INSERT INTO `sys_role_user` VALUES ('1', '1');
+INSERT INTO `sys_role_user` VALUES ('2', '2');
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'admin', 'admin');
+INSERT INTO `sys_user` VALUES ('2', 'abel', 'abel');
